2a7f3727d05559572b6466cbe00e2c272bd80f09
davnt <davidnmoyo@outlook.com>
2021-10-13 18:06:57 +0200

    initial com

--- README.md
+++ README.md
@@ -1,70 +1,99 @@
-# Getting Started with Create React App
+<!-- Please update value in the {}  -->
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<h1 align="center">Input component </h1>
 
-## Available Scripts
+<div align="center">
+   Solution for a challenge from  <a href="http://devchallenges.io" target="_blank">Devchallenges.io</a>.
+</div>
 
-In the project directory, you can run:
+<div align="center">
+  <h3>
+    <a href="https://input-comp.netlify.app/">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="https://github.com/DavNt/frontend-inputs">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenges/TSqutYM4c5WtluM7QzGp">
+      Challenge
+    </a>
+  </h3>
+</div>
 
-### `npm start`
+<!-- TABLE OF CONTENTS -->
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Table of Contents
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+- [Overview](#overview)
+  - [Built With](#built-with)
+- [Features](#features)
+- [How to use](#how-to-use)
+- [Contact](#contact)
+- [Acknowledgements](#acknowledgements)
 
-### `npm test`
+<!-- OVERVIEW -->
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Overview
 
-### `npm run build`
+![screenshot](./src/resources/screenshot/screenshot-1.png)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+Introduce your projects by taking a screenshot or a gif. Try to tell visitors a story about your project by answering:
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+- Where can I see your demo?
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+  <a href="https://input-comp.netlify.app/">
+    Demo
+  </a>
 
-### `npm run eject`
+- What was your experience?
+  
+  Using React and Bootstrap components 
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+- Your wisdom? :)
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+  Experience is gained through doing something.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+### Built With
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+<!-- This section should list any major frameworks that you built your project using. Here are a few examples.-->
 
-## Learn More
+- [React](https://reactjs.org/)
+- [CSS](https://www.w3schools.com/css/default.asp)
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+## Features
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+<!-- List the features of your application or follow the template. Don't share the figma file here :) -->
 
-### Code Splitting
+This application/site was created as a submission to a [DevChallenges](https://devchallenges.io/challenges) challenge. The [challenge](https://devchallenges.io/challenges/TSqutYM4c5WtluM7QzGp) was to build an application to complete the given user stories.
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+## How To Use
 
-### Analyzing the Bundle Size
+<!-- Example:  -->
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+To clone and run this application, you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
 
-### Making a Progressive Web App
+```bash
+# Clone this repository
+$ git clone https://github.com/DavNt/frontend-inputs
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+# Install dependencies
+$ npm install
 
-### Advanced Configuration
+# Run the app
+$ npm start
+```
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+## Acknowledgements
 
-### Deployment
+<!-- This section should list any articles or add-ons/plugins that helps you to complete the project. This is optional but it will help you in the future. For exmpale -->
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+- [Steps to replicate a design with only HTML and CSS](https://devchallenges-blogs.web.app/how-to-replicate-design/)
+- [Node.js](https://nodejs.org/)
+- [Marked - a markdown parser](https://github.com/chjj/marked)
 
-### `npm run build` fails to minify
+## Contact
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- GitHub [@DavNt](https://github.com/DavNt)
+- Twitter [@DavNtM](https://twitter.com/DavNtM)
---
+++ src/components/input.component.js
@@ -0,0 +1,305 @@
+import React, {Component} from 'react';
+import Phoneic from '../resources/icons/ic_local_phone_black_36dp.png';
+import Lockic from '../resources/icons/ic_lock_outline_black_36dp.png';
+
+export default class InputSec extends Component{
+  render(){
+    return(
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-3 bg_grey">
+            <div className="row mx-5 mt-4 mb-5 fw-bold">
+              <p>
+                <span className="dev_color">Dev</span>Challenges.io
+              </p>
+            </div>
+            <div className="row mx-4 my-3">Colors</div>
+            <div className="row mx-4 my-3">Typography</div>
+            <div className="row mx-4 my-3">Spaces</div>
+            <div className="row mx-4 my-3">Buttons</div>
+            <div className="row mx-4 my-3 fw-bold">Inputs</div>
+            <div className="row mx-4 my-3">Grid</div>
+          </div>
+
+          <div className="col-sm-9">
+            <h3 className="m-4 mt-5">Inputs</h3>
+            <div className="row">
+              <div className="col-md-2 m-3">
+                <label
+                 for="textbox1" 
+                 className="form-label"
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" className="input_field hov" 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                />
+              </div>
+              <div className="col-md-2 m-3 ms-5">
+                <label
+                 for="textbox1" 
+                 className="form-label blck-txt"
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" className="input_field blck-input" 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                />
+              </div>
+              <div className="col-md-2 m-3 ms-5">
+                <label
+                 for="textbox1" 
+                 className="form-label blu-txt"
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" className="input_field blu-input" 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-2 m-3">
+                <label
+                 for="textbox1" 
+                 className="form-label error-txt"
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" className="input_field error-input" 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                />
+              </div>
+              <div className="col-md-2 m-3 ms-5">
+                <label
+                 for="textbox1" 
+                 className="form-label blck-txt "
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" className="input_field blck-input " 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                />
+              </div>
+              <div className="col-md-2 m-3 ms-5">
+                <label
+                 for="textbox1" 
+                 className="form-label error-txt"
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" className="input_field error-input " 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-2 m-3">
+                <label
+                 for="textbox1" 
+                 className="form-label"
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" className="input_field" 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                  disabled
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-2 m-3">
+                <label
+                 for="textbox1" 
+                 className="form-label"
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" className="input_field hov" 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                />
+                <small className="">Interesting text</small>
+              </div>
+              <div className="col-md-2 m-3 ms-5">
+                <label
+                 for="textbox1" 
+                 className="form-label error-txt"
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" className="input_field error-input" 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                />
+                <small className="error-txt">Interesting text</small>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-2 m-3 ">
+                  <label
+                  for="textbox1" 
+                  className="form-label"
+                  >
+                    Label
+                  </label>
+                  <div className="input-group">
+                    <i class=" ic-add">
+                      <img 
+                        src={Phoneic} 
+                        alt="" 
+                        className="img-reponsive icon-img"
+                      />
+                    </i>
+                    <input 
+                      type="text" className="input_field txt_indent hov" 
+                      id="textbox1" 
+                      placeholder="Placeholder"
+                    />
+                  </div>
+                </div>
+              <div className="col-md-2 m-3 ms-5">
+                  <label
+                  for="textbox1" 
+                  className="form-label"
+                  >
+                    Label
+                  </label>
+                  <div className="input-group">
+                    <input 
+                      type="text" className="input_field hov" 
+                      id="textbox1" 
+                      placeholder="Placeholder"
+                    />
+                    <i class=" ic-add ">
+                      <img 
+                        src={Lockic} 
+                        alt="" 
+                        className="img-reponsive icon-img ic_right"
+                      />
+                    </i>
+                  </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-2 m-3">
+                <label
+                 for="textbox1" 
+                 className="form-label"
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" className="input_field hov" 
+                  id="textbox1" 
+                  value="Text"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-2 m-3">
+                <label
+                 for="textbox1" 
+                 className="form-label"
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" 
+                  className="input_field form-control-sm hov" 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                />
+              </div>
+              <div className="col-md-2 m-3 ms-5">
+                <label
+                 for="textbox1" 
+                 className="form-label"
+                >
+                  Label
+                </label>
+                <input 
+                  type="text" 
+                  className="input_field form-control-md hov" 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-10 m-3">
+                <label
+                 for="textbox1" 
+                 className="form-label"
+                >
+                  Label
+                </label><br/>
+                <input 
+                  type="text" className="input_field full_width hov" 
+                  id="textbox1" 
+                  value="Text"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+            <div className="col-md-2 m-3">
+                <label
+                 for="textbox1" 
+                 className="form-label"
+                >
+                  Label
+                </label>
+                <textarea 
+                  type="text" className="input_field hov" 
+                  id="textbox1" 
+                  placeholder="Placeholder"
+                  rows="3"
+                />
+              </div>
+            </div>
+
+            {/* <div className="row">
+              <div className="col-md-2 m-3">
+                <button className="btn btn-light light_hover">Default</button>
+              </div>
+              <div className="col-md-2 m-3">
+                <button className="btn btn-primary">Default</button>
+              </div>
+              <div className="col-md-2 m-3">
+                <button className="btn btn-secondary">Default</button>
+              </div>
+              <div className="col-md-2 m-3">
+                <button className="btn btn-danger">Default</button>
+              </div>
+            </div>
+ */}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
